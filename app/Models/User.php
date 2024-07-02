a360286f40b91249e02c1572bf852928975132c5
Gustavo Delgado <gdquevedo@gmail.com>
2024-07-01 22:41:58 -0600

    Modelo user completo

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,44 +2,31 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    use  HasFactory, Notifiable;
+    
     protected $fillable = [
         'full_name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+   
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
+    
+    protected $casts = [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
@@ -58,5 +45,5 @@ class User extends Authenticatable
         public function comments(){
             return $this->hasMany(Comment::class);
         }
-    }
 }
+
