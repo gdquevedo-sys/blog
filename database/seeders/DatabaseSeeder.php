7692acc3aabddc1ca3ea52eaa24ad8e09e43861d
Gustavo Delgado <gdquevedo@gmail.com>
2024-07-02 22:42:42 -0600

    Todos los modelos creados con datos de prueba con factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
+use App\Models\Comment;
+use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +18,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //Eliminar carpeta articles y categories
+        Storage::deleteDirectory('articles');
+        Storage::deleteDirectory('categories');
+
+        //Crear carpeta donde se almacenaran las imagenes
+        Storage::makeDirectory('articles');
+        Storage::makeDirectory('categories');
+       
         //LLamar al seeder
         $this->call(UserSeeder::class);
+
+        //Llamar a los factories
+        Category::factory(8)->create();
+        Article::factory(20)->create();
+        Comment::factory(20)->create();
     }
 }
