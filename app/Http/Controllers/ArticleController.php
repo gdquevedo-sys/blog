ba57f9587e59672daf3e7afcc1e4c65f8b6f27bb
Gustavo Delgado <gdquevedo@gmail.com>
2024-07-08 22:30:41 -0600

    Creacion del controlador de articulos parte 2

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,6 +6,8 @@ use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
 {
@@ -67,22 +69,61 @@ class ArticleController extends Controller
 
     public function show(Article $article)
     {
-        //
+        $comments = $article->comments()->simplePaginate(5);
+
+        return view('subscriber.articles.show', compact('article', 'comments'));
     }
 
     public function edit(Article $article)
     {
-        //
+        //Obtener categorias publicas
+        $categories = Category::select(['id', 'name'])
+                                ->where('status', '1')
+                                ->get();
+
+        return view('admin.articles.edit', compact('categories', 'article'));   
     }
 
-    public function update(Request $request, Article $article)
+    public function update(ArticleRequest $request, Article $article)
     {
-        //
+        //Si el usuario sube una nueva imagen
+        if($request->hasFile('image')){
+            //Eliminar la imagen anterior
+            File::delete(public_path('storage/' . $article->image));
+            //Asignar la nueva imagen
+            $article['image'] = $request->file('image')->store('articles');
+        }
+
+        //Actualizar datos
+        $article->update([
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'introduction' => $request->introduction,
+            'body' => $request->body,
+            'user_id' => Auth::user()->id,
+            'category_id' => $request->category_id,
+            'status' => $request->status,
+        ]);
+
+        //Redireccionamos al index
+        return redirect()->action([ArticleController::class, 'index'])
+                            ->with('success-update', 'Artículo modificado con éxito');
+
     }
 
 
     public function destroy(Article $article)
     {
-        //
+        //Eliminar imagen del articulo
+        if($artile->image){
+            File::delete(public_path('storage/' . $article->image));
+        }
+
+        //Eliminar articulo
+        $article->delete();
+
+        //Redireccionamos al index
+        return redirect()->action([ArticleController::class, 'index'], compact('article'))
+                            ->with('success-delete', 'Artículo eliminado con éxito');
     }
 }
